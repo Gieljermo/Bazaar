1400d7f51b118fe376bbf8df3a4499123da90858
Gieljermo <gielvanrossum@gmail.com>
2024-02-28 10:00:41 +0100

    added seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,8 +13,51 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Giel',
+        DB::table('users')->insert([
+            [
+                'name' => 'admin',
+                'lastname' => '-',
+                'street' => '-',
+                'house_number' => '-',
+                'postal_code' => '-',
+                'email' => 'admin@live.nl',
+                'password' => Hash::make('admin'),
+                'role_id' => 4,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Jan',
+                'lastname' => 'Maas',
+                'street' => 'Volgeslaan',
+                'house_number' => '13',
+                'postal_code' => '1234AB',
+                'email' => 'maas@live.nl',
+                'password' => Hash::make('password'),
+                'role_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Kira',
+                'lastname' => 'Molen',
+                'street' => 'Huisstraat',
+                'house_number' => '11',
+                'postal_code' => '1234AB',
+                'email' => 'molen@live.nl',
+                'password' => Hash::make('password'),
+                'role_id' => 2,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Lilian',
+                'lastname' => 'Akkers',
+                'street' => 'Graanlaan',
+                'house_number' => '12',
+                'postal_code' => '1234AB',
+                'email' => 'akkers@live.nl',
+                'password' => Hash::make('password'),
+                'role_id' => 3,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
         ]);
     }
 }
