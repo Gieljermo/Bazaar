4014db4570562512ac3b40f04f69811ff6c9c147
Gieljermo <gielvanrossum@gmail.com>
2024-03-10 17:17:36 +0100

    styling

--- resources/views/Listings/index.blade.php
+++ resources/views/Listings/index.blade.php
@@ -4,19 +4,23 @@
 ])
 
 @section('content')
-    @foreach ($listings as $listing)
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">{{$listing->product->product_name}}</h5>
-                <p class="card-text">{{$listing->product->description}}</p>
-                @if ($listing->type == "bidding")
-                    <p class="card-text">Bieden tot: {{$listing->bid_until}}</p>
-                @else
-                    <p class="card-text">&euro;{{$listing->price}}</p>
-                @endif
-                <a href="{{Route('listings.show', $listing->id)}}" class="btn btn-primary">Advertentie bekijken</a>
+    <div class="w-50 d-flex justify-content-start flex-wrap">
+        @foreach ($listings as $listing)
+        <div class="card-container">
+            <div class="card" style="width: 18rem;">
+                <img class="p-1 card-img-top" src="{{$listing->getImageUrl()}}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{$listing->product->product_name}}</h5>
+                    <p class="card-text">{{$listing->product->description}}</p>
+                    @if ($listing->type == "bidding")
+                        <p class="card-text">Bieden tot: {{$listing->bid_until}}</p>
+                    @else
+                        <p class="card-text">&euro;{{$listing->price}}</p>
+                    @endif
+                    <a href="{{Route('listings.show', $listing->id)}}" class="btn btn-primary">Advertentie bekijken</a>
+                </div>
             </div>
         </div>
-    @endforeach
+        @endforeach
+    </div>
 @endsection
