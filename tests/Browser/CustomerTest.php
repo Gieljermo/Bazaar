e320acbba2c99d7137c14df6fc77e62a6eb9331e
Gieljermo <gielvanrossum@gmail.com>
2024-06-05 15:26:23 +0200

    test

--- tests/Browser/CustomerTest.php
+++ tests/Browser/CustomerTest.php
@@ -5,17 +5,30 @@ namespace Tests\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
+use App\Models\Purchase;
+use App\Models\User;
+use App\Models\Listing;
+use Illuminate\Support\Carbon;
 
 class CustomerTest extends DuskTestCase
 {
     /**
      * A Dusk test example.
      */
-    public function testExample(): void
+    public function test_user_sees_purchase_history(): void
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+        $user = User::find(4);
+
+        $listing = Listing::where('ended', 0)->where('purchase_id', null)->orderBy('id', 'desc')->first();
+
+        $this->browse(function (Browser $browser) use ($user, $listing) {
+            $browser->loginAs($user)
+                ->visitRoute("listings.index")
+                ->clickLink("Advertenties")
+                ->click("#link_".$listing->product->id)
+                ->click("#buy-product")
+                ->visit('/customer/purchases')
+                ->assertSee($listing->product->product_name);
         });
     }
 }
