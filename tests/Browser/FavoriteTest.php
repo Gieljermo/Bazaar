0abeffba76eff989995621370e71c7a4df3a4406
Thampie2185577 <p.anthonyrajah@student.avans.nl>
2024-06-06 15:25:46 +0200

    6-6-2024
    2 extra dusk test functies toegevoegd voor favorieten

--- tests/Browser/FavoriteTest.php
+++ tests/Browser/FavoriteTest.php
@@ -29,10 +29,12 @@ class FavoriteTest extends DuskTestCase
 
     }
 
+
     public function test_user_add_favorite_advertisement_with_login (): void
     {
         $product = Product::find(1);
         $user = User::find(2);
+
 
         $this->browse(function (Browser $browser) use ($product, $user) {
             $browser->logout()->loginAs($user)
@@ -72,4 +74,40 @@ class FavoriteTest extends DuskTestCase
         });
     }
 
+
+    public function test_user_see_empty_favorite_advertisement_list_with_login (): void
+    {
+        $user = User::find(2);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->logout()->loginAs($user)
+                ->visitRoute("customer.favorites")
+                ->assertSee("FAVORIETEN VAN ".(strtoupper($user->name)." ".strtoupper($user->lastname)))
+                ->pause(500);;
+        });
+
+    }
+
+    public function test_user_see_favorite_advertisement_list_with_login (): void
+    {
+        $user = User::find(2);
+        $product = Product::find(1);
+
+        Favorite::insert([
+            'user_id' => $user->id,
+            "listing_id" => Listing::where("product_id", $product->id)->first()->id
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->logout()->loginAs($user)
+                ->visitRoute("customer.favorites")
+                ->assertSee("FAVORIETEN VAN ".(strtoupper($user->name)." ".strtoupper($user->lastname)))
+                ->pause(1000);;
+        });
+
+        Favorite::where([
+            'user_id' => $user->id,
+            "listing_id" => Listing::where("product_id", $product->id)->first()->id
+        ])->delete();
+    }
 }
